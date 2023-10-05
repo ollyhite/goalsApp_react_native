97728b10f48a52ac11365a929eed14d08e4c07a5
PeiYun <ollylee8520@gmail.com>
2023-10-05 15:30:53 -0600

    add onChangeText/onPress function and map the goals list

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
+import { useState } from "react";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  const [inputText, setInputText] = useState("");
+  const [goals, setGoal] = useState([]);
+  function goalInputHandler(enteredText) {
+    setInputText(enteredText);
+  }
+
+  function addGoalHandler() {
+    setGoal((currentGoals) => [...currentGoals, inputText]);
+    setInputText("");
+  }
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Your goal" style={styles.textInput} />
-        <Button title="Add Goal" />
+        <TextInput
+          placeholder="Your goal"
+          style={styles.textInput}
+          onChangeText={goalInputHandler}
+          value={inputText}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <Text>Lists of goals...</Text>
+        {goals.map((goal) => (
+          <Text style={styles.goalItem} key={goal}>
+            {goal}
+          </Text>
+        ))}
       </View>
     </View>
   );
@@ -38,5 +58,13 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5,
+    flexDirection: "column",
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#009999",
+    color: "white",
   },
 });
